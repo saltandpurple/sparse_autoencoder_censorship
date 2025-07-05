a6cdc68250fbf5ca7d6143bd6e6a5653289a1ab9
Matthias Baum <d0ghouse@freenet.de>
2025-07-05 11:10:19 +0200

    implemented local openai-like api

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,6 +1,6 @@
 from typing import Dict, Any, List, TypedDict, Annotated
 from langchain_openai import ChatOpenAI
-from langchain_community.chat_models import BedrockChat
+from langchain_aws import ChatBedrock
 from langchain_core.runnables import add
 from langgraph.graph import StateGraph, END
 from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
@@ -17,31 +17,37 @@ DEFAULT_REGION = "us-east-1"
 
 class PromptGenerator:
     def __init__(self):
+        # clients
         self.bedrock_client = boto3.client(
             service_name="bedrock-runtime",
             region_name=DEFAULT_REGION
         )
         self.lmstudio_client = ChatOpenAI(
-            base_url="http://192.168.5.61/v1",
-            model_kwargs={"temperature": 0.7}
+            base_url="http://192.168.178.61:1234/v1"
             )
 
-        self.prompt_generator = BedrockChat(
+        # models
+        self.prompt_generator = ChatBedrock(
             client=self.bedrock_client,
-            model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+            model="anthropic.claude-3-sonnet-20240229-v1:0",
             model_kwargs={"temperature": 0.7}
         )
-        self.subject = BedrockChat(
-            client=self.bedrock_client,
-            model_id="deepseek-r1:7b",
-            model_kwargs={"temperature": 0.7}
+        self.subject = ChatOpenAI(
+            client=self.lmstudio_client,
+            model="deepseek/deepseek-r1-0528-qwen3-8b",
+            temperature=0.7
         )
-        self.evaluator = BedrockChat(
+        self.evaluator = ChatBedrock(
             client=self.bedrock_client,
-            model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+            model="anthropic.claude-3-sonnet-20240229-v1:0",
             model_kwargs={"temperature": 0.7}
         )
 
+    def test_local(self):
+        prompt = "What is the capital of France?"
+        response = self.subject.invoke([HumanMessage(content=prompt)])
+        print(response.content)
 
 if __name__ == "__main__":
     prompt_generator = PromptGenerator()
+    prompt_generator.test_local()
