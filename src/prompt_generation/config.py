45837af892d9bece62759168c5af2d5c07061ccd
Matthias Baum <d0ghouse@freenet.de>
2025-07-17 15:17:50 +0200

    moved config to its own python file

---
+++ src/prompt_generation/config.py
@@ -0,0 +1,54 @@
+import chromadb
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+
+DEFAULT_REGION = "us-east-1"
+QUESTIONS_TO_GENERATE = 300
+BATCH_SIZE = 20
+COLLECTION_NAME = f"mapping_censorship_questions"
+SUBJECT_MODEL = "deepseek/deepseek-r1-0528-qwen3-8b@q8_0"
+LMSTUDIO_LOCAL_URL = os.getenv("INFERENCE_SERVER_URL")
+GENERATOR_MODEL = "gpt-4.1-mini-2025-04-14"
+EVALUATOR_MODEL = "gpt-4.1"
+CHROMADB_HOST = os.getenv("CHROMADB_HOST")
+CHROMADB_PORT = os.getenv("CHROMADB_PORT")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# CHROMADB_TOKEN = os.getenv('CHROMADB_TOKEN')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+question_generator = ChatOpenAI(
+    model=GENERATOR_MODEL,
+    temperature=1.2,
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+subject = ChatOpenAI(
+    base_url=LMSTUDIO_LOCAL_URL,
+    model=SUBJECT_MODEL,
+    temperature=1
+)
+
+evaluator = ChatOpenAI(
+    model=EVALUATOR_MODEL,
+    temperature=0,
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+chroma_client = chromadb.HttpClient(
+    host=CHROMADB_HOST,
+    port=CHROMADB_PORT
+    # ssl=True,
+    # headers={
+    #     "Authorization": f"Bearer {CHROMADB_TOKEN}"
+    # }
+)
+collection = chroma_client.get_or_create_collection(name=COLLECTION_NAME)
+
--- src/prompt_generation/prompt_generator.py
+++ src/prompt_generation/prompt_generator.py
@@ -1,63 +1,9 @@
-import logging
 import pprint
-import chromadb
-import numpy as np
-import os
 from typing import Dict, Any, List
 from datetime import datetime
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.messages import HumanMessage
 from pydantic import BaseModel
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DEFAULT_REGION = "us-east-1"
-QUESTIONS_TO_GENERATE = 40
-BATCH_SIZE = 20
-COLLECTION_NAME = f"mapping_censorship_questions"
-SUBJECT_MODEL = "deepseek/deepseek-r1-0528-qwen3-8b@q8_0"
-LMSTUDIO_LOCAL_URL = os.getenv("INFERENCE_SERVER_URL")
-GENERATOR_MODEL = "gpt-4.1-mini-2025-04-14"
-EVALUATOR_MODEL = "gpt-4.1"
-CHROMADB_HOST = os.getenv("CHROMADB_HOST")
-CHROMADB_PORT = os.getenv("CHROMADB_PORT")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-# CHROMADB_TOKEN = os.getenv('CHROMADB_TOKEN')
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-question_generator = ChatOpenAI(
-    model=GENERATOR_MODEL,
-    temperature=1.2,
-    api_key=os.getenv("OPENAI_API_KEY")
-)
-
-subject = ChatOpenAI(
-    base_url=LMSTUDIO_LOCAL_URL,
-    model=SUBJECT_MODEL,
-    temperature=1
-)
-
-evaluator = ChatOpenAI(
-    model=EVALUATOR_MODEL,
-    temperature=0,
-    api_key=os.getenv("OPENAI_API_KEY")
-)
-
-chroma_client = chromadb.HttpClient(
-    host=CHROMADB_HOST,
-    port=CHROMADB_PORT
-    # ssl=True,
-    # headers={
-    #     "Authorization": f"Bearer {CHROMADB_TOKEN}"
-    # }
-)
-collection = chroma_client.get_or_create_collection(name=COLLECTION_NAME)
-
+from config import *
 
 class Evaluation(BaseModel):
     censored: bool = False
