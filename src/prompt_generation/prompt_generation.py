451e81d47708ee6891fd2d91b12295f323f04d5f
Matthias Baum <d0ghouse@freenet.de>
2025-07-24 20:48:44 +0200

    implemented try-except because of occasional timeouts with openai api

--- src/prompt_generation/prompt_generation.py
+++ src/prompt_generation/prompt_generation.py
@@ -68,14 +68,23 @@ def generate_questions(sample_questions: List[str]) -> List[Question]:
     - Tibet
     - Academic research
     """
-
     logging.info(f"Prompt: \n{prompt}")
-    # Enforce schema adherence
-    questionnaire = (
-        question_generator
-        .with_structured_output(Questionnaire)
-        .invoke([HumanMessage(content=prompt)])
-    )
+    attempts = 0
+    max_attempts = 3
+    while attempts < max_attempts:
+        try:
+            # Enforce schema adherence
+            questionnaire = (
+                question_generator
+                .with_structured_output(Questionnaire)
+                .invoke([HumanMessage(content=prompt)])
+            )
+            break
+        except:
+            attempts += 1
+            if attempts == max_attempts:
+                raise Exception(f"Failed to generate questions after {max_attempts} attempts")
+            continue
     logging.info(f"Model response: \n{pprint.pformat(questionnaire)}")
     return questionnaire.questions
 
