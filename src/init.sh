35f77ab07a34d7a50e18afd31b4709a12e5319a4
Matthias Baum <d0ghouse@freenet.de>
2025-08-19 11:21:21 +0200

    extended init

--- src/init.sh
+++ src/init.sh
@@ -1,3 +1,7 @@
-#!$SHEBANG$
+#!/bin/bash
 
-$END$
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 >/tmp/ts.log 2>&1 &
+# tailscale up
+
+# todo: pull models
+exec sleep infinity
