ef83bb74de625da3c08b72fe6daf47bf72a24b71
Matthias Baum <d0ghouse@freenet.de>
2025-08-21 12:30:10 +0200

    attempting to work around ridiculous saelens limitations

--- src/sae_training/train.py
+++ src/sae_training/train.py
@@ -5,7 +5,7 @@ from sae_lens import (
     LanguageModelSAERunnerConfig,
     LanguageModelSAETrainingRunner,
     TopKTrainingSAEConfig,
-    LoggingConfig,
+    LoggingConfig
 )
 from transformer_lens import HookedTransformer
 from transformer_lens.utils import get_act_name
@@ -34,37 +34,37 @@ NUM_FEATURES = 40 # adjust after testing
 
 # --------------
 
-hf_model = AutoModelForCausalLM.from_pretrained(
-    MODEL_PATH,
-    torch_dtype="bfloat16",
-    local_files_only=True
-)
-
-model = HookedTransformer.from_pretrained_no_processing(
-    MODEL_ALIAS,
-    hf_model=hf_model,
-    device="cuda",
-    torch_dtype=torch.bfloat16,
-    local_files_only=True
-)
+# hf_model = AutoModelForCausalLM.from_pretrained(
+#     MODEL_PATH,
+#     torch_dtype="bfloat16",
+#     local_files_only=True
+# )
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code=True)
+#
+# model = HookedTransformer.from_pretrained_no_processing(
+#     MODEL_ALIAS,
+#     hf_model=hf_model,
+#     tokenizer=tokenizer,
+#     device="cuda",
+#     torch_dtype=torch.bfloat16,
+#     local_files_only=True
+# )
 
 cfg = LanguageModelSAERunnerConfig(
     model_name="Qwen/Qwen3-8b", # required, fails otherwise
     hook_name=TARGET_HOOK,
     training_tokens=TOTAL_TRAINING_TOKENS,
-    # act_store_device="cpu", # saves VRAM, but reduces speed
     # use_cached_activations=False,
     # cached_activations_path=ACTIVATIONS_PATH,
-    dataset_trust_remote_code=True, # deprecated
     dataset_path="cerebras/SlimPajama-627B",
     # context_size=512,
     streaming=True,
-    model_from_pretrained_kwargs={
-        "local_files_only": True,
-        "hf_model": hf_model,
-        "dtype": "bfloat16",
-        "trust_remote_code": True
-    },
+    # model_from_pretrained_kwargs={
+    #     "local_files_only": True,
+    #     "hf_model": hf_model,
+    #     "dtype": "bfloat16",
+    #     "trust_remote_code": True
+    # },
 
     sae= TopKTrainingSAEConfig(
         d_in=MODEL_HIDDEN_D,
@@ -94,4 +94,5 @@ cfg = LanguageModelSAERunnerConfig(
     checkpoint_path="checkpoints",
     dtype="float32",
 )
-sparse_autoencoder = LanguageModelSAETrainingRunner(cfg, override_model=model).run()
+# sparse_autoencoder = LanguageModelSAETrainingRunner(cfg, override_model=model).run()
+sparse_autoencoder = LanguageModelSAETrainingRunner(cfg).run()
